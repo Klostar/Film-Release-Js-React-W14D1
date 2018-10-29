76bcfc8fceb2bb2597e06a4ea14fb6b06d5c1219
chloe alcorn <ca.alcorn@outlook.com>
2018-10-29 15:26:10 +0000

    can access information from the film box componewnt

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import FilmBox from './components/FilmBox';
 
 class App extends Component {
   render() {
     return (
-<h1> hello world! </h1>
+<FilmBox />
     );
   }
 }
---
+++ src/components/FilmBox.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+//impport film list
+
+class FilmBox extends Component{
+render(){
+  return(
+    <div className = 'film-box'>
+
+    <h2>hello</h2>
+    </div>
+  )
+}
+}
+
+export default FilmBox;
